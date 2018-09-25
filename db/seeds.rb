7365794eced90b1162b111e24425889714bb678f
Koen Handekyn <koen.handekyn@up-nxt.com>
2018-09-25 21:36:15 +0200

    cleaning up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+koen = Player.create!(name: 'koen handekyn', email: 'koen@handekyn.com')
+gert = Player.create!(name: 'gert de gelder', email: 'koen@handekyn.com')
+yves = Player.create!(name: 'yves breemersch', email: 'koen@handekyn.com')
+kevin = Player.create!(name: 'kevin doorakkers', email: 'koen@handekyn.com')
+
+spain = Game.create!('spain', p1: koen, p2: gert, p3: yves, p4: kevin)
+Round.create!(game: spain, s1: 2, s2: 2, s3: -2, s3: -2, roundtype: 'normal')
